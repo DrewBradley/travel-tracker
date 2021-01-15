2413aba7a68a7598b9d1ee3d0a9514f01f6650fc
Drew Bradley <williamandrewb@gmail.com>
2021-01-15 16:07:16 -0700

    Add class method for finding a traveler's trip data by userID.

--- src/Trips.js
+++ src/Trips.js
@@ -2,6 +2,14 @@ class Trip {
   constructor(data) {
     this.data = data;
   }
+  findUserTrips(user) {
+    let userTrips = this.data.filter(element => {
+      return element.userID === user
+    })
+    return userTrips
+  }
+
+
 }
 
 export default Trip;
--- src/index.js
+++ src/index.js
@@ -16,17 +16,60 @@ import {
   deleteTrip
 } from './utility.js';
 
+
 const dashboardGreeting = document.querySelector('.dashboard-greeting')
+const pastTrips = document.querySelector('.dashboard-trips-past')
+const pastTripTemplate = document.querySelector('.past-trips-template')
+const upcomingTrips = document.querySelector('.dashboard-trips-future')
+const upcomingTripTemplate = document.querySelector('.future-trips-template')
 
 const pageLoad = () => {
   let rando = (Math.ceil(Math.random() * 49))
   getTraveler(rando)
-    .then(traveler => traveler = new Traveler(traveler.id, traveler.name, traveler.travelerType))
-    .then(traveler => dashboardGreeting.innerText = "Well Hello There, " + (traveler.returnFirstNameLastInitial()))
+  .then(traveler => traveler = new Traveler(traveler.id, traveler.name, traveler.travelerType))
+  .then(traveler => dashboardGreeting.innerText = "Hello, " + (traveler.returnFirstNameLastInitial()))
   getTrips()
-    .then(trips => console.log(trips))
+    .then(trips => trips = new Trips(trips))
+    .then(trips => trips.findUserTrips(rando))
+    .then(trips => displayUserTrips(trips))
   getDestinations()
     .then(destination => console.log(destination))
+}
+
+const findUpcomingTrips = (today, trips) => {
+  let futureTrips = trips.filter(trips => {
+    return trips.date > today
+  })
+  if (futureTrips.length) {
+    return futureTrips
+  } else {
+    return "You have no upcoming trips!"
+  }
+}
+
+const findPastTrips = (today, trips) => {
+  let pastTrips = trips.filter(trips => {
+    return trips.date < today
+  })
+  return pastTrips
+}
+
+const displayUserTrips = (trips) => {
+  let today = new Date().toISOString().slice(0,10)
+  let upcoming = findUpcomingTrips(today, trips);
+  let past = findPastTrips(today, trips);
+  console.log(past)
+  past.forEach(trip => {
+    let tripCard = pastTripTemplate.cloneNode(true);
+    pastTrips.appendChild(tripCard);
+    console.log(trip)
+    pastTripTemplate.querySelector('.trip-info').textContent = `${trip}`
+  })
+  // upcoming.forEach(trip => {
+  //   let tripCard = upcomingTripTemplate.cloneNode(true);
+  //   upcomingTrips.appendChild(tripCard);
+  //   tripCard.querySelector('.trip-info').innerText = `${trip.id}`
+  // })
 }
 
 window.onload = pageLoad();
