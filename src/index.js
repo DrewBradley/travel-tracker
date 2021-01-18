b6119b0008b9ecffe38103d1e93f5c5dabd2fdaf
Drew Bradley <williamandrewb@gmail.com>
2021-01-18 11:52:06 -0700

    Configure CSS grid to display trip description.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 import './images/ExcursiOnward-logo.png'
+import './images/map-background.jpg'
 
 import Trip from './Trips';
 import Destination from './Destination'
